68cfa1b0d767bea5477329300035fe64aaaea43d
PKHong <pyungkang1004@gmail.com>
2021-03-26 14:34:05 +0900

    git ignore

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,15 @@ FROM python:3.9.0
 
 WORKDIR /home/
 
-RUN git clone https://github.com/PyungkangHong094/FD_Holdings.git
+RUN git clone https://github.com/PyungkangHong094/fdfam.git
 
-WORKDIR /home/
+WORKDIR /home/fdhomepage/
+
+RUN pip install -r  requirements.txt
+
+RUN python manage.py migrate
+
+EXPOSE 8000
+
+CMD ["python","manage.py","runserver","0.0.0.0:8000"]
+
--- fdhomepage/settings.py
+++ fdhomepage/settings.py
@@ -11,16 +11,27 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# reading .env file
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pxrrxsbks&-*4@)f4tjss*okko#evf_p9@8&4@!0$lj9wjxpp7'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
